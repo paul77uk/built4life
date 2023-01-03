e1bebad8d57adb92ba83088c7c502f5b7bd5274a
Paul Vickers <paul77uk@gmail.com>
2023-01-03 21:07:48 +0000

    progressing on days

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,5 +1,6 @@
 class DaysController < ApplicationController
   before_action :set_workout
+  before_action :set_day, only: %i[edit update destroy]
 
   def new
     @day = @workout.days.build
@@ -15,7 +16,27 @@ class DaysController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @day.update(day_params)
+      redirect_to workout_path(@workout), notice: 'Day was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @day.destroy
+
+    redirect_to workout_path(@workout)
+  end
+
   private
+
+  def set_day
+    @day = @workout.days.find(params[:id])
+  end
 
   def day_params
     params.require(:day).permit(:title)
